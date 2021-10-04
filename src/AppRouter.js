d4801b92517653a39e6af8a7ef719996a25030c2
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-10-04 11:26:48 +0300

    Get nomics pai key and setup browse components

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -4,14 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 import { Landing } from './components/landing';
 import { Login } from './components/login';
 import { Logout } from './components/logout';
-import { Home } from './components/home';
 import { Navbar } from './components/navbar';
+import { Browse } from './components/browse';
 
 export const AppRouter = () => {
   return (
     <Switch>
       <Route exact path="/" component={Landing} />
-      <Route path="/home" component={() => <Navbar main={<Home />} />} />
+      <Route path="/browse" component={() => <Navbar main={<Browse />} />} />
       <Route path="/login" component={Login} />
       <Route path="/logout" component={Logout} />
     </Switch>
