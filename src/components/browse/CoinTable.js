0612fdbf36644ecafc39fb1b5f4f896666e028c2
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-10-04 13:41:56 +0300

    Add coin table

---
+++ src/components/browse/CoinTable.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
+
+export const CoinTable = ({ coins }) => {
+  return (
+    <div>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>#</Th>
+            <Th>Logo</Th>
+            <Th>Symbol</Th>
+            <Th>Name</Th>
+            <Th isNumeric>Price</Th>
+            <Th isNumeric>1d price</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {coins.map((coin) => {
+            return (
+              <Tr key={coin.id}>
+                <Td>{coin.rank}</Td>
+                <Td>
+                  <img src={coin.logo_url} alt={coin.id} width="24px"></img>
+                </Td>
+                <Td>{coin.symbol}</Td>
+                <Td>{coin.name}</Td>
+                <Td isNumeric>${Number(coin.price).toFixed(2)}</Td>
+                <Td
+                  isNumeric
+                  color={
+                    Number(coin['1d'].price_change_pct) < 0
+                      ? 'red.400'
+                      : 'green.400'
+                  }
+                >
+                  {coin['1d'].price_change_pct}%
+                </Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </div>
+  );
+};
--- src/components/browse/index.js
+++ src/components/browse/index.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Spinner } from '@chakra-ui/react';
+
+import { CoinTable } from './CoinTable';
 
 export const Browse = () => {
-  return <h1>Hello team</h1>;
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const perPage = 10;
+  const baseUrl = 'https://api.nomics.com/v1/currencies/ticker';
+  const URL = `${baseUrl}?key=${process.env.REACT_APP_NOMICS_API_KEY}&per-page=${perPage}&page=${page}&interval=1d`;
+
+  useEffect(() => {
+    const getCoins = () => {
+      setLoading(true);
+
+      fetch(URL)
+        .then((res) => res.json())
+        .then((res) => {
+          setCoins(res);
+          setLoading(false);
+        });
+    };
+
+    getCoins();
+  }, [URL]);
+
+  return <div>{loading ? <Spinner /> : <CoinTable coins={coins} />}</div>;
 };
